3ce115b9cd93b0bc4fc234e82ad26b3438814336
dani <dani@mandarine.(none)>
2009-07-22 20:55:34 +0200

    projects

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,13 @@ class Item < ActiveRecord::Base
 	belongs_to :item_image
   has_many :item_images, :foreign_key => :item_id, :order => 'listpos'
 
-	def self.find_projects(years)
-		items = []
-		years.each do |year|
-			Item.find(:all, :conditions => ['category = ? AND subcategory = ?', 'projects', year], 
-				:order => 'position').each {|i| items << i}
-		end
-		items
-	end
+  has_one :main_image,  :foreign_key => :item_id, :order => 'listpos', :conditions => {:position => 'index image'}
+
+
+  def self.find_projects(years)
+    Item.all(:include => :item_images, :order => 'subcategory DESC, position ASC',
+      :conditions => {:category => 'projects', :subcategory => years})
+  end
 
 	def front_image
     ItemImage.find_by_item_id_and_position(id, "main image")
@@ -24,7 +23,7 @@ class Item < ActiveRecord::Base
 		ItemImage.find_by_item_id_and_position(id, "index image")
 	end
 
-	def main_image
+	def no_main_image
 		ItemImage.find_by_item_id_and_position(id, "main image")
 	end
 
