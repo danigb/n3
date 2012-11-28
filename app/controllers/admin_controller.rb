caf86e05ae78bff4bc172a85b6c8198023ededc4
danigb <danigb@gmail.com>
2012-11-28 11:19:49 +0100

    Encoding problems

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@
 class AdminController < ApplicationController
   layout "admin"
   before_filter :login_required
+  before_filter :force_utf8_params
 
   # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
   verify :method => :post, :only => [ :destroy, :create, :update, :destroy_image ],
@@ -110,5 +111,23 @@ class AdminController < ApplicationController
         item.item_images << ItemImage.new(:uploaded_data => attachment) unless attachment.size == 0
       end
     end
+  end
+
+  # https://rails.lighthouseapp.com/projects/8994/tickets/4336-ruby19-submitted-string-form-parameters-with-non-ascii-characters-cause-encoding-errors
+  def force_utf8_params
+    traverse = lambda do |object, block|
+      if object.kind_of?(Hash)
+        object.each_value { |o| traverse.call(o, block) }
+      elsif object.kind_of?(Array)
+        object.each { |o| traverse.call(o, block) }
+      else
+        block.call(object)
+      end
+      object
+    end
+    force_encoding = lambda do |o|
+      o.force_encoding(Encoding::UTF_8) if o.respond_to?(:force_encoding)
+    end
+    traverse.call(params, force_encoding)
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 require 'yaml' 
 YAML::ENGINE.yamler = 'syck'
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
